a715a6eba9e53efca7694a12ce0e9bc181aee54b
Kython Shu <kythonshu@gmail.com>
2017-04-21 15:22:02 +0800

    Initial Commit

---
+++ GradientLayerTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GradientLayerTest
+//
+//  Created by Kython Shu on 2017/4/21.
+//  Copyright © 2017年 Kython Shu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GradientLayerTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GradientLayerTest
+//
+//  Created by Kython Shu on 2017/4/21.
+//  Copyright © 2017年 Kython Shu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
